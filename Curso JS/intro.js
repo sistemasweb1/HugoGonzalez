ab0f0101c07eaf350f2ef1f77c60387cf39749c6
Hugo Alejandro González Herrera <al137705@alumnos.uacj.mx>
2018-11-08 14:51:00 -0700

    JavaScript tutorial

--- Curso JS/intro.js
+++ Curso JS/intro.js
@@ -1,3 +1,9 @@
 function myExternalFunction(){
     document.getElementById("demo8").innerHTML = "Parrafo externo hecho con amor y JacaScript..";
 }
+
+function stateFunction(){
+    document.getElementById("test1").innerHTML = "Hello Dolly!";
+    
+    document.getElementById("test2").innerHTML = "How are you?";
+}
